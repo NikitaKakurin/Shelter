472c58fe24becdf0f93cd6f70553c9be9ca40f20
NikitaKakurin <neyasut87@rambler.ru>
2022-04-24 22:44:58 +0300

    fix: fix problems with images in developvent mode.

--- shelter/src/js/allPets.js
+++ shelter/src/js/allPets.js
@@ -1,8 +1,17 @@
+import charly from "../assets/image/slider/pets-charly.png";
+import freddie from "../assets/image/slider/pets-freddie.png";
+import jennifer from "../assets/image/slider/pets-jennifer.png";
+import katrine from "../assets/image/slider/pets-katrine.png";
+import scarlet from "../assets/image/slider/pets-scarlet.png";
+import sophia from "../assets/image/slider/pets-sophia.png";
+import timmy from "../assets/image/slider/pets-timmy.png";
+import woody from "../assets/image/slider/pets-woody.png";
+
 export const allPets = [
     {
       "id":"0",
       "name": "Jennifer",
-      "img": "../assets/image/slider/pets-jennifer.png",
+      "img": jennifer,
       "type": "Dog",
       "breed": "Labrador",
       "description": "Jennifer is a sweet 2 months old Labrador that is patiently waiting to find a new forever home. This girl really enjoys being able to go outside to run and play, but won't hesitate to play up a storm in the house if she has all of her favorite toys.",
@@ -14,7 +23,7 @@ export const allPets = [
     {
       "id":"1",
       "name": "Sophia",
-      "img": "../assets/image/slider/pets-sophia.png",
+      "img": sophia,
       "type": "Dog",
       "breed": "Shih tzu",
       "description": "Sophia here and I'm looking for my forever home to live out the best years of my life. I am full of energy. Everyday I'm learning new things, like how to walk on a leash, go potty outside, bark and play with toys and I still need some practice.",
@@ -26,7 +35,7 @@ export const allPets = [
     {
       "id":"2",
       "name": "Woody",
-      "img": "../assets/image/slider/pets-woody.png",
+      "img": woody,
       "type": "Dog",
       "breed": "Golden Retriever",
       "description": "Woody is a handsome 3 1/2 year old boy. Woody does know basic commands and is a smart pup. Since he is on the stronger side, he will learn a lot from your training. Woody will be happier when he finds a new family that can spend a lot of time with him.",
@@ -38,7 +47,7 @@ export const allPets = [
     {
       "id":"3",
       "name": "Scarlett",
-      "img": "../assets/image/slider/pets-scarlet.png",
+      "img": scarlet,
       "type": "Dog",
       "breed": "Jack Russell Terrier",
       "description": "Scarlett is a happy, playful girl who will make you laugh and smile. She forms a bond quickly and will make a loyal companion and a wonderful family dog or a good companion for a single individual too since she likes to hang out and be with her human.",
@@ -50,7 +59,7 @@ export const allPets = [
     {
       "id":"4",
       "name": "Katrine",
-      "img": "../assets/image/slider/pets-katrine.png",
+      "img": katrine,
       "type": "Cat",
       "breed": "British Shorthair",
       "description": "Katrine is a beautiful girl. She is as soft as the finest velvet with a thick lush fur. Will love you until the last breath she takes as long as you are the one. She is picky about her affection. She loves cuddles and to stretch into your hands for a deeper relaxations.",
@@ -62,7 +71,7 @@ export const allPets = [
     {
       "id":"5",
       "name": "Timmy",
-      "img": "../assets/image/slider/pets-timmy.png",
+      "img": timmy,
       "type": "Cat",
       "breed": "British Shorthair",
       "description": "Timmy is an adorable grey british shorthair male. He loves to play and snuggle. He is neutered and up to date on age appropriate vaccinations. He can be chatty and enjoys being held. Timmy has a lot to say and wants a person to share his thoughts with.",
@@ -74,7 +83,7 @@ export const allPets = [
     {
       "id":"6",
       "name": "Freddie",
-      "img": "../assets/image/slider/pets-freddie.png",
+      "img": freddie,
       "type": "Cat",
       "breed": "British Shorthair",
       "description": "Freddie is a little shy at first, but very sweet when he warms up. He likes playing with shoe strings and bottle caps. He is quick to learn the rhythms of his human’s daily life. Freddie has bounced around a lot in his life, and is looking to find his forever home.",
@@ -86,7 +95,7 @@ export const allPets = [
     {
       "id":"7",
       "name": "Charly",
-      "img": "../assets/image/slider/pets-charly.png",
+      "img": charly,
       "type": "Dog",
       "breed": "Jack Russell Terrier",
       "description": "This cute boy, Charly, is three years old and he likes adults and kids. He isn’t fond of many other dogs, so he might do best in a single dog home. Charly has lots of energy, and loves to run and play. We think a fenced yard would make him very happy.",
